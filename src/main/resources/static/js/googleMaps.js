a4d84e1d8bf338be1dc8c3b62bd90deb914075cf
ha1den1 <rokasdelinda1@gmail.com>
2024-06-17 12:20:21 +0300

    veikiantis markeriai

--- src/main/resources/static/js/googleMaps.js
+++ src/main/resources/static/js/googleMaps.js
@@ -6,6 +6,21 @@ export function initMap() {
         center: vilnius,
     });
 
+    // Predefined locations
+    const locations = [
+        { lat: 54.722470, lng: 25.289500, name: 'Tire change' },
+        { lat: 54.722037, lng: 25.295260, name: 'Autoservice' },
+        { lat: 54.722470, lng: 25.281500, name: 'Stop-Servis' },
+        { lat: 54.6840, lng: 25.2829, name: 'Car Fix' },
+        { lat: 54.6951, lng: 25.2753, name: 'Speedy Repair' }
+    ];
+
+    // Create markers for predefined locations
+    locations.forEach(location => {
+        createMarker(location);
+    });
+
+    // Nearby search for car repair places
     const service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
         location: vilnius,
@@ -14,18 +29,36 @@ export function initMap() {
     }, function(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             results.forEach(place => {
-                createMarker(place);
+                createMarker({
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng(),
+                    name: place.name
+                });
             });
         } else {
             alert('Places service was not successful for the following reason: ' + status);
         }
     });
 
-    function createMarker(place) {
-        new google.maps.Marker({
+    // Function to create a marker
+    function createMarker(location) {
+        const marker = new google.maps.Marker({
             map,
-            position: place.geometry.location,
-            title: place.name
+            position: { lat: location.lat, lng: location.lng },
+            title: location.name
+        });
+
+        // Info window content
+        const infoWindowContent = `<h3>${location.name}</h3><p>Lat: ${location.lat}, Lng: ${location.lng}</p>`;
+
+        // Create info window
+        const infoWindow = new google.maps.InfoWindow({
+            content: infoWindowContent
+        });
+
+        // Add click listener to marker
+        marker.addListener('click', () => {
+            infoWindow.open(map, marker);
         });
     }
 }
